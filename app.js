760be7ea608fffd6f8fa77cbdeea42c2f0b41c0d
Marta-asturias <marta.gonzalez@domoticadavinci.com>
2022-05-25 22:22:26 +0200

    acabado email

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-function pruebaemail (valor){
+function email (valor){
 	re=/^([\da-z_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/
 	if(!re.exec(valor)){
-		alert('email no valido');
+		alert('email no valido por favor ingrese un email valido');
 	}
-	else alert('email valido');
+	else alert('usted acaba de registrarse gracias');
 	}
